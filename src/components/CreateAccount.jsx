07f3e4cce23ed4c4778ab957c82bc338c500a2b0
Harsh Dhobe <harshdhobe123@gmail.com>
2024-11-03 00:17:40 +0530

    warning added

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { Button, Card } from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
+
+
 
 const CreateAccount = () => {
-  return (
-    <div>
-      <h2>CreateAccount</h2>
-    </div>
-  )
+    return (
+        <div className='content'>
+
+            <div className='mnemonic'>
+
+                <ExclamationCircleOutlined style={{ fontSize: "20px" }} />
+                <div>
+                    Once you generate the seed phrase, save it securely in order to
+                    recover your wallet in the future.
+                </div>
+
+
+            </div>
+
+        </div>
+    )
 }
 
 export default CreateAccount
