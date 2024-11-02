c3732fb5fdaaaf2b22b7618e3c80e3eeedb81095
Harsh Dhobe <harshdhobe123@gmail.com>
2024-11-03 00:45:03 +0530

    add create wallet page

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Button, Card } from "antd";
 import { ExclamationCircleOutlined } from "@ant-design/icons";
+import { useNavigate } from 'react-router-dom';
 
 
 
 const CreateAccount = () => {
+    const navigate = useNavigate();
     return (
         <div className='content'>
 
@@ -15,9 +17,17 @@ const CreateAccount = () => {
                     Once you generate the seed phrase, save it securely in order to
                     recover your wallet in the future.
                 </div>
-
-
             </div>
+            <Button type="primary" className='frontPageButton'>Generate Seed Phrase</Button>
+            <Card className='seedPhraseContainer'></Card>
+            <Button type="primary" className='frontPageButton'>Open Your New Wallet</Button>
+            
+            <p className='frontPageBottom'
+                onClick={() => navigate("/")}
+            >Back Home</p>
+
+
+
 
         </div>
     )
