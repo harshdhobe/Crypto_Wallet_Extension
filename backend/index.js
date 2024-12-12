3a877721cce5ea9565bcfec0e2c244d6244326d1
Harsh Dhobe <harshdhobe123@gmail.com>
2024-12-12 20:12:21 +0530

    add final routes

--- backend/index.js
+++ backend/index.js
@@ -31,15 +31,26 @@ app.get("/getTokens", async (req, res) => {
         "mediaItems": true
     });
 
-    const jsonResponse = {
-        tokens: tokens.raw,
-        nfts: nfts.raw
-    };
 
+const myNfts = nfts.raw.result.map((e, i) => {
+    if (e?.media?.media_collection?.high?.url && !e.possible_spam && (e?.media?.category !== "video")) {
+        return e["media"]["media_collection"]["high"]["url"];
+    }
+})
+
+const balance = await Moralis.EvmApi.balance.getNativeBalance({
+    chain: chain,
+    address: userAddress
+});
+
+const jsonResponse = {
+    tokens: tokens.raw,
+    nfts: myNfts,
+    balance: balance.raw.balance / (10 ** 18)
+}
 
     return res.status(200).json(jsonResponse);
 });
-
 
 
 Moralis.start({
